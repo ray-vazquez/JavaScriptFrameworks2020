e38d715bd066a8521e63137ab210a66a4cb1729c
Louis345 <jt125845@gmail.com>
2020-02-17 16:47:50 -0500

    WIP

---
+++ exercises/06_use_effect_challenge/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [allCharacters, setAllCharacters] = useState();
+  const [image, setImage] = useState(
+    "https://i.ytimg.com/vi/UFFi9PWKDjg/maxresdefault.jpg"
+  );
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios(
+        "https://rickandmortyapi.com/api/character/"
+      );
+      console.log({ response });
+      setAllCharacters(response.data.results);
+    }
+    getData();
+  }, []);
+
+  async function getCharacter(characterId) {
+    try {
+      const response = await axios(
+        `https://rickandmortyapi.com/api/character/${characterId}`
+      );
+      setImage(response.data.image);
+    } catch (error) {
+      console.log({ error });
+    }
+  }
+  return (
+    <div className="container">
+      <div className="row text-center" id="body">
+        <h1 id="title-head">Rick Sanchez</h1>
+        <div id="main-img">
+          <a href="http://rickandmorty.wikia.com/wiki/Rick_Sanchez">
+            <img
+              className="img-rounded"
+              alt="Get Schwifty"
+              id="get-schwifty"
+              src={image}
+              width="500"
+              height="281"
+            />
+          </a>
+          <p id="photo-caption">
+            <em>Rick and his homies, gettin' schwifty.</em>
+          </p>
+          <div className="linkfooter">
+            <p>Select your favorite character</p>
+            <select
+              id="dropdown"
+              type="text"
+              onChange={e => getCharacter(e.currentTarget.value)}
+            >
+              {allCharacters &&
+                allCharacters.map((character, index) => (
+                  <option value={index + 1} data-id={index} key={character.id}>
+                    {character.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
